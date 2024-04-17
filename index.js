b710662bd14a9a471b2bb59531afb7d7517b3efc
Bobby Rockers <rrockers@gmail.com>
2024-04-17 18:01:09 -0500

    Initial push to repo of our color console.

---
+++ index.js
@@ -0,0 +1,4 @@
+import { color, log, red, green } from 'console-log-colors';
+
+console.log(color.red("This is a NOT boring message."));
+console.log(color.green("This is REALLY NOT boring message."));
